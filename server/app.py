f59770237012485a0006711863652025d833a0e3
Michael Kiptoo <michaelkiptoo2000@gmail.com>
2024-03-13 16:18:36 +0300

    Routes

--- server/app.py
+++ server/app.py
@@ -1,88 +1,32 @@
 from flask import Flask, make_response, request, jsonify, abort, render_template
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_cors import  CORS
+from flask_jwt_extended import JWTManager,jwt_required,get_jwt_identity
 from werkzeug.exceptions import NotFound
 from models import db, User, Parcel, Delivery, Location, UserNotification
-
+from auth import auth_bp
 app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = b'\xb2\xd3B\xb9 \xab\xc0By\x13\x10\x84\xb7M!\x11'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///SendIT.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.json.compact = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
 api = Api(app)
+CORS(app)
+jwt = JWTManager()
+jwt.init_app(app)
+
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
+# @app.errorhandler(NotFound)
+# def handle_not_found(e):
+#     return render_template('index.html', title='Homepage', message='Welcome to SendIT')
 
 
-@app.errorhandler(NotFound)
-def handle_not_found(e):
-    return render_template('index.html', title='Homepage', message='Welcome to SendIT')
-
-class UsersList(Resource):
-    def get(self):
-        users = User.query.all()
-        return [{"id": user.id, "username": user.username, "email":user.email} for user in users] 
-
-    def post(self):
-        data = request.get_json()
-        username = data['username']
-        email =data ['email']
-        password = data['password']
-
-
-        if not username:
-            abort(400, description="Username is required.")
-
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_user:
-            abort(400, description="User with this username already exists.")
-
-        new_user = User(username=username, email=email, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        response = make_response(jsonify(new_user.serialize()), 201)
-        return response
-
-api.add_resource(UsersList, "/users")
-
-class UsersByID(Resource):
-    def get(self, user_id):
-        user = User.query.get(user_id)
-        if user:
-            return {"id": user.id, "username": user.username, "email":user.email}
-        else:
-            raise NotFound("User not found")
-        
-    def delete(self, user_id):
-        user = User.query.get(user_id)
-        if not user:
-            return {'error':'User does not exist'},404
-        else:
-            db.session.delete(user)
-            db.session.commit()
-            response = make_response(jsonify({'Message':'User deleted'}), 200)
-            return response
-    
-    def patch(self, user_id):
-
-        existing_user = User.query.get(user_id)
-        if not existing_user:
-            return {'error':'User does not exist'},404
-        
-        data = request.get_json()
-        if 'username' in data:
-            existing_user.username = data['username']
-        elif 'email' in data:
-            existing_user.email = data['email']
-        else:
-            return {'error':'No field to update provided'},400
-
-        db.session.commit()
-
-        response = make_response(jsonify(existing_user.serialize()), 200)
-        return response
 class ParcelResource(Resource):
     def get(self, parcel_id):
         parcel = Parcel.query.get_or_404(parcel_id)
@@ -301,7 +245,6 @@ api.add_resource(LocationResource, "/locations/<int:location_id>")
 api.add_resource(LocationsList, "/locations")
 api.add_resource(UserNotificationResource, "/notifications/<int:notification_id>")
 api.add_resource(UserNotificationsList, "/notifications")
-api.add_resource(UsersByID, "/users/<int:user_id>")
 
 
 
